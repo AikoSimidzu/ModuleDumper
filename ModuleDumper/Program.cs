ca29458de1483b84ed1277c636497ee42d484171
Yato29 <egor.puchoK@yandex.ru>
2020-08-14 22:09:13 +0300

    Upload

---
+++ ModuleDumper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModuleDumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name process: ");
+            string prname = Console.ReadLine();
+            Process proc = Process.GetProcessesByName(prname)[0];
+            ProcessModuleCollection modules = proc.Modules;
+
+            foreach (ProcessModule module in modules)
+            {
+                File.AppendAllText(Application.StartupPath.ToString() + $"\\{prname}Dump.log", $"Name: {module.ModuleName}\nMemorySize: {module.ModuleMemorySize}\nBaseAddress: {module.BaseAddress}\nEntryPointAddress: {module.EntryPointAddress}\n------------------\n");
+            }
+        }
+    }
+}
